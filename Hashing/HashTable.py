cf56fc6f60752acc99c6911ccb61fb830f1f5e27
Aka-Ayaan <am08853@st.habib.edu.pk>
2024-04-06 15:31:03 +0500

    Done Q2

--- Hashing/HashTable.py
+++ Hashing/HashTable.py
@@ -56,7 +56,7 @@ def resize_hashtable(hashtable,size,increase): #return hashtable,size
                     newtable[1][hashvalue] = data
                 else:
                     newhash = collision_resolver(key,hashvalue,size)
-                    while newtable[0][newhash] != None and newtable[0][newhash] != key and hashtable[0][newhash] != '#':
+                    while newtable[0][newhash] != None and newtable[0][newhash] != key:
                         newhash = collision_resolver(key,newhash,size)
                     if newtable[0][newhash] == None:
                         newtable[0][newhash] = key
@@ -99,16 +99,21 @@ def put(hashtable,key, data,size): #return hashtable,size
     if hashtable[0][hashvalue] == None:
         hashtable[0][hashvalue] = key
         hashtable[1][hashvalue] = data
+    
     else:
         if hashtable[0][hashvalue] == key:
             hashtable[1][hashvalue] = data
+    
         else:
             newhash = collision_resolver(key,hashvalue,size)
+    
             while hashtable[0][newhash] != None and hashtable[0][newhash] != key and hashtable[0][newhash] != '#':
                 newhash = collision_resolver(key, newhash, size)
+    
             if hashtable[0][newhash] == None:
                 hashtable[0][newhash] = key
                 hashtable[1][newhash] = data
+    
             else:
                 if hashtable[0][newhash] == key:
                     hashtable[1][newhash] = data
@@ -123,54 +128,80 @@ def loadFactor(hashtable,size): # returns a float - Loadfactor of hashtable
     count = 0
     
     for i in hashtable[0]:
-        if i is not None:
+        if i is not None and i != '#':
             count += 1
 
     return count / size
 
-def Update(hashtable,key, columnName, data,size,collision_path,opNumber): # returns Nothing, prints 'record Updated'
-    pass
+def Update(hashtable,key, columnName, data, size, collision_path, opNumber): # returns Nothing, prints 'record Updated'
+    
+    start = hash_function(key,size)
+    pos = start
+    collision_path[opNumber] = []
+
+    while hashtable[0][pos] != None and hashtable[0][pos] != '#':
         
+            if hashtable[0][pos] == key:
+                hashtable[1][pos][columnName] = data
+                collision_path[opNumber].append(pos)
+                return 'record Updated'
+        
+            else:
+                collision_path[opNumber].append(pos)
+                pos = collision_resolver(key, pos, size)
+                if pos == start:
+                    return 'Item not found'
+                
 def get(hashtable,key,size,collision_path,opNumber): # returns dictionary
 
     start = hash_function(key,size)
     pos = start
+    collision_path[opNumber] = []
 
-    while hashtable[0][pos] != None and hashtable[0][pos] != '#':
-    
-        if hashtable[0][pos] == key:
-            return hashtable[1][pos]
-    
-        else:
-            collision_path[opNumber].append(pos)
-            pos = collision_resolver(key, pos, size)
-            if pos == start:
-                return 'Item not found'
+    while hashtable[0][pos] != None:
             
-    print(collision_path)
-
-
+            if hashtable[0][pos] == key:
+                collision_path[opNumber].append(pos)
+                return collision_path
+            
+            else:
+                collision_path[opNumber].append(pos)
+                pos = collision_resolver(key, pos, size)
+                if pos == start:
+                    return 'Item not found'
+                
+    return collision_path
         
 def delete(hashtable, key, size,collision_path,opNumber): #returns hashtable, size, prints a msg  'Item Deleted'
     
     newhash = 0
-    count = 0
     hashvalue = hash_function(key,size)
+    collision_path[opNumber] = []
     
+    collision_path[opNumber].append(hashvalue)
+
     if hashtable[0][hashvalue] != key:
         newhash = collision_resolver(key, hashvalue, size)
+        collision_path[opNumber].append(newhash)
         
-        while hashtable[0][newhash] != key and count != size:
-            collision_path[opNumber].append(newhash)
+        while hashtable[0][newhash] != key:
+            if newhash not in collision_path[opNumber]:
+                collision_path[opNumber].append(newhash)
             newhash = collision_resolver(key, newhash,size)
         
         if hashtable[0][newhash] == key:
             hashtable[0][newhash] = "#"
             hashtable[1][newhash] = "#"
+            print('Item Deleted')
     
     else:
         hashtable[0][hashvalue] = "#"
         hashtable[1][hashvalue] = "#"
+        print('Item Deleted')
 
-    if loadFactor(hashtable,size) < 0.25:
+
+    print(loadFactor(hashtable,size))
+    if loadFactor(hashtable,size) < 0.30:
         hashtable,size = resize_hashtable(hashtable,size,False)
+
+    return hashtable, size    
